b11ef5c62d678170732e3edfd81230e6afcd420d
phillsatellite <pprints12@gmail.com>
2025-07-15 23:12:39 -0400

    Scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+//function creates a variable and assigns it the FlatBurger Value
+function addBurger(){
+    const newBurger = "Flatburger";
+    burgers.push(newBurger); //Adds Flatburger to the burgers array
+
+    if(true){
+        const anotherNewBurger = "Maple Bacon Burger"; //creates a block-scoped variable and assigns its value "Maple Bacon Burger"
+        burgers.push(anotherNewBurger); //Adds Maple Bacon Burger to the burgers array
+    }
+}
+
+//Changes the value of featuredDrink to "The JavaShake"
+function changeFeaturedDrink(){
+    featuredDrink = "The JavaShake";
+}
